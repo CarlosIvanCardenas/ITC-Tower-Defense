a575a7700af70c4b7b30a0867c1b59ee2d7c4083
Diego Valadez <diegoiv_1995@hotmail.com>
2016-11-14 13:48:44 -0600

    Adding files

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public float speed = 10f;
+
+	private Transform target;
+	private int wavepointIndex = 0;
+
+	void Start()
+	{
+		target = Waypoints.points [0];
+	}
+
+	void Update ()
+	{
+		Vector3 dir = target.position - transform.position;
+		transform.Translate (dir.normalized * speed * Time.deltaTime, Space.World);
+
+		if (Vector3.Distance (transform.position, target.position) <= 0.4f) 
+		{
+			GetNextWaypoint ();
+			RotateEnemy();
+		}
+	}
+
+	void GetNextWaypoint()
+	{
+		if (wavepointIndex >= Waypoints.points.Length - 1) 
+		{
+			Destroy (gameObject);
+			return;
+		}
+
+		wavepointIndex++;
+		target = Waypoints.points [wavepointIndex];
+	}
+
+	void RotateEnemy ()
+	{
+		switch (wavepointIndex) 
+		{
+		case 1:
+			transform.Rotate (new Vector3(0,-90,0));
+			break;
+		case 2:
+			transform.Rotate (new Vector3(0,90,0));
+			break;
+		case 3:
+			transform.Rotate (new Vector3(0,90,0));
+			break;
+		case 4:
+			transform.Rotate (new Vector3(0,-90,0));
+			break;
+		case 5:
+			transform.Rotate (new Vector3(0,-90,0));
+			break;
+		case 6:
+			transform.Rotate (new Vector3(0,-90,0));
+			break;
+		case 7:
+			transform.Rotate (new Vector3(0,-90,0));
+			break;
+		case 8:
+			transform.Rotate (new Vector3(0,90,0));
+			break;
+		case 9:
+			transform.Rotate (new Vector3(0,90,0));
+			break;
+		case 10:
+			transform.Rotate (new Vector3(0,90,0));
+			break;
+		}
+	}
+
+}
---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+	public Transform enemyPrefav;
+	public Transform spawnPoint;
+	public float timeBetweenWaves = 5f;
+	public float countdown = 2f;
+
+	private int waveNumber = 1;
+
+	void Update ()
+	{
+		if (countdown <= 0f) 
+		{
+			SpawnWave ();
+			countdown = timeBetweenWaves;
+		}
+
+		countdown -= Time.deltaTime;
+
+	}
+
+	void SpawnWave ()
+	{
+		Debug.Log ("Wave Incoming!");
+		for (int i = 0; i < waveNumber; i++) 
+		{
+			SpawnEnemy ();
+		}
+		waveNumber++;
+	}
+
+	void SpawnEnemy ()
+	{
+		Instantiate (enemyPrefav, spawnPoint.position, spawnPoint.rotation);
+	}
+}
